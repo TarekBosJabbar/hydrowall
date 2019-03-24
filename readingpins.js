b8132dc0e821f3cda0b2820f5db486d9a57d15a7
Tarek <bosjabbar.tarek@gmail.com>
2019-03-23 23:52:54 -0700

    New code to test input

---
+++ readingpins.js
@@ -0,0 +1,41 @@
+var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+
+var input1 = new Gpio(11, 'in' , 'both'); //use GPIO pin 4, and specify that it is output
+
+var input2 = new Gpio(9, 'in', 'both');
+
+var input3 = new Gpio(10, 'in', 'both');
+
+var input4 = new Gpio(8, 'in', 'both');
+
+input1.watch((err, value) => {
+  if (err) {
+    throw err;
+  }
+ 
+  console.log("input1 " + value);
+});
+
+input2.watch((err, value) => {
+  if (err) {
+    throw err;
+  }
+ 
+  console.log("input2 " + value);
+});
+
+input3.watch((err, value) => {
+  if (err) {
+    throw err;
+  }
+ 
+  console.log("input3 " + value);
+});
+
+input4.watch((err, value) => {
+  if (err) {
+    throw err;
+  }
+ 
+  console.log("input4 " + value);
+});
