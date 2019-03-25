f0e6e7e558cb645720f2948ad704fb7292330af9
Tarek <bosjabbar.tarek@gmail.com>
2019-03-24 17:22:35 -0700

    bug fix

--- tempRead.js
+++ tempRead.js
@@ -22,7 +22,7 @@ const mcp3008 = spi.open(0, 0, (err) => {
 
     console.log(voltage);
 
-    const ph  = (voltage - .41412) / -5.916;
+    const ph  = (voltage - .41412) / -.05916;
 
     console.log("the ph is" + ph);
   });
