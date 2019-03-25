7cc363b23be6f9e95f5905c0872874df1c23f2c7
Tarek <bosjabbar.tarek@gmail.com>
2019-03-24 18:22:16 -0700

    mcpTest.js add

---
+++ mcpTest.js
@@ -0,0 +1,25 @@
+const mcpadc = require('mcp-spi-adc');
+
+const pHSensor = mcpadc.open(1, {speedHz: 20000}, (err) => {
+  if (err) throw err;
+
+  setInterval(() => {
+    pHSensor.read((err, reading) => {
+      if (err) throw err;
+
+      console.log((reading.value * 3.3 - 0.5) * 100);
+    });
+  }, 1000);
+
+});
+const ECSensor = mcpadc.open(3, {speedHz: 20000}, (err) => {
+  if (err) throw err;
+
+  setInterval(() => {
+    pHSensor.read((err, reading) => {
+      if (err) throw err;
+
+      console.log((reading.value * 3.3 - 0.5) * 100);
+    });
+  }, 1000);
+});
