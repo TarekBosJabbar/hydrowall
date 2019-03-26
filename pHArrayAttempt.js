84d92b79c82c5dbdf710505357ce625564cf9d03
Tarek <bosjabbar.tarek@gmail.com>
2019-03-25 22:53:45 -0700

    back to basics for mcpTest

---
+++ pHArrayAttempt.js
@@ -0,0 +1,56 @@
+// This is mostly a test code in an attempt to get analog sensors to interface with a raspberry
+
+const mcpadc = require('mcp-spi-adc');
+
+
+
+const pHSensor = mcpadc.open(0, {speedHz: 20000}, (err) => {
+  if (err) throw err;
+
+  setInterval(() => {
+    pHSensor.read((err, reading) => {
+      if (err) throw err;
+
+      console.log("pH Sensor " + (reading.value));
+    });
+  }, 1000);
+
+});
+
+/*
+const ECSensor = mcpadc.open(2, {speedHz: 20000}, (err) => {
+  if (err) throw err;
+
+  setInterval(() => {
+    pHSensor.read((err, reading) => {
+      if (err) throw err;
+
+      console.log("pH sensor " + (-reading.value - .41412 / (-.596)));
+    });
+  }, 1000);
+});
+
+const tempSensor = mcpadc.open(4, {speedHz: 20000}, (err) => {
+  if (err) throw err;
+
+  setInterval(() => {
+    tempSensor.read((err, reading) => {
+      if (err) throw err;
+
+      console.log("temp sensor " + (reading.value));
+    });
+  }, 1000);
+});
+
+const testSensor = mcpadc.open(7, {speedHz: 20000}, (err) => {
+  if (err) throw err;
+
+  setInterval(() => {
+    testSensor.read((err, reading) => {
+      if (err) throw err;
+
+      console.log("test sensor " + (reading.value));
+    });
+  }, 1000);
+});
+*/
